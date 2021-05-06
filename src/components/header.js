a22aaef79d3af0a5f4dc89181655a16c7750005d
Dmitry Vdovichenko <dmitry.vdovichencko@gmail.com>
2021-05-06 10:00:47 +0500

    header styling finished

--- src/components/header.js
+++ src/components/header.js
@@ -5,15 +5,19 @@ import HeaderStyles from "./header.module.scss"
 
 const Header = ({ siteTitle }) => (
   <header className={HeaderStyles.header}>
-      <h1>
+      
         <Link to="/">
-          <span>{`${siteTitle} `} </span>
-          <span role="img" aria-label="memo">📝</span>
-          <span role="img" aria-label="book">📖</span>
-          <span role="img" aria-label="mortar_board">🎓</span>
-          <span role="img" aria-label="computer">💻</span>
+					<div className={HeaderStyles.header__container}>
+          <h1 className={HeaderStyles.header__title}>{`${siteTitle} `} </h1>
+					<span className={HeaderStyles.header__icons}>
+						<span role="img" aria-label="memo">📝</span>
+						<span role="img" aria-label="book">📖</span>
+						<span role="img" aria-label="mortar_board">🎓</span>
+						<span role="img" aria-label="computer">💻</span>
+					</span>
+					</div>
         </Link>
-      </h1>
+      
       <nav className={HeaderStyles.nav}>
         <div className={HeaderStyles.nav__item}><Link to="/tags/js">JavaScript</Link></div>
         <div className={HeaderStyles.nav__item}><Link to="/tags/react">React</Link></div>
